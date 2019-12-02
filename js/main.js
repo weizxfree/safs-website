16dcf7a04ec4df0c2a26a0ff5ea62d853d7e46c9
weizhangxiang <weizhangxiang@sensorsdata.cn>
2019-12-02 19:04:24 +0800

    添加了动画列表

--- js/main.js
+++ js/main.js
@@ -64,3 +64,51 @@ $(document).ready(function(){
         });      
     })();
 });
+
+
+ $(function(){
+    var aLi = $('#brand .bd-box li');
+    var aImg =  $('#brand .bd-box li div.just img');
+    var aSpan = $('#brand .bd-box li div.against img');
+    var aSpann = $('#brand .bd-box li div.against');
+    aLi.each(function(index){
+        $(this).mouseover(function(){
+            aSpan.eq(index).stop();
+            aSpann.eq(index).stop();
+            aImg.eq(index).stop();
+            aImg.eq(index).css({zIndex:1}).animate({
+                left:177.5,
+                width:0
+            },80,'',function(){
+                $(this).hide();
+                aSpan.eq(index).show().css({zIndex:3}).animate({
+                    left:0,
+                    width:355
+                },80)
+                aSpann.eq(index).show().css({zIndex:4}).animate({
+                    left:0,
+                    width:355
+                },80)
+            })
+        })
+        $(this).mouseout(function(){
+            aSpan.eq(index).stop();
+            aSpann.eq(index).stop();
+            aImg.eq(index).stop();
+            aSpan.eq(index).css({zIndex:1}).animate({
+                left:177.5,
+                width:0
+            },80,''),
+            aSpann.eq(index).css({zIndex:1}).animate({
+                left:177.5,
+                width:0
+            },80,'',function(){
+                $(this).hide();
+                aImg.eq(index).show().css({zIndex:2}).animate({
+                    left:0,
+                    width:355
+                },80)
+            })
+        })
+    })
+  })
