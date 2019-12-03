f2d5dbc255131ce7ee31756563b6860bc886d48b
weizhangxiang <weizhangxiang@sensorsdata.cn>
2019-12-03 11:48:07 +0800

    图片拉伸

--- js/main.js
+++ js/main.js
@@ -77,17 +77,17 @@ $(document).ready(function(){
             aSpann.eq(index).stop();
             aImg.eq(index).stop();
             aImg.eq(index).css({zIndex:1}).animate({
-                left:177.5,
+                left:250.5,
                 width:0
             },80,'',function(){
                 $(this).hide();
                 aSpan.eq(index).show().css({zIndex:3}).animate({
                     left:0,
-                    width:355
+                    width:500
                 },80)
                 aSpann.eq(index).show().css({zIndex:4}).animate({
                     left:0,
-                    width:355
+                    width:500
                 },80)
             })
         })
@@ -96,17 +96,17 @@ $(document).ready(function(){
             aSpann.eq(index).stop();
             aImg.eq(index).stop();
             aSpan.eq(index).css({zIndex:1}).animate({
-                left:177.5,
+                left:250.5,
                 width:0
             },80,''),
             aSpann.eq(index).css({zIndex:1}).animate({
-                left:177.5,
+                left:250.5,
                 width:0
             },80,'',function(){
                 $(this).hide();
                 aImg.eq(index).show().css({zIndex:2}).animate({
                     left:0,
-                    width:355
+                    width:500
                 },80)
             })
         })
